7dec8e4a4cdf021f9913047e5bc45adb1b7a0522
kieferrm <kieferrm@gmail.com>
2017-04-27 15:01:37 -0700

    generate problems for many files

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,9 @@
 import * as vscode from 'vscode';
 
 
-const NUMBER_OF_ERRORS = 5000;
+const NUMBER_OF_ERRORS_LARGE = 5000;
+const NUMBER_OF_ERRORS_SMALL = 2;
+const NUMBER_OF_FILES = 10000;
 const NUMBER_OF_MESSAGES = 100000;
 const NUMBER_OF_MESSAGES_PER_CHANNEL = 50000;
 const NUMBER_OF_TERMINAL_MESSAGES = 10000;
@@ -68,19 +70,26 @@ export function activate(context: vscode.ExtensionContext) {
         }
     }
 
-    function floodDiagnostics(editor: vscode.TextEditor = vscode.window.activeTextEditor, numberOfErrors: number = NUMBER_OF_ERRORS) {
+    function floodDiagnostics(editor: vscode.TextEditor = vscode.window.activeTextEditor, numberOfErrors: number = NUMBER_OF_ERRORS_LARGE, numberOfFiles: number = 1) {
         if (editor) {
-            let source = `crazy-${Date.now()}`;
+            let source = `crazy-${Date.now()}`;            
             let diagnosticCollection: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection(source);
             diagnosticCollections.push(diagnosticCollection);
-            let diagnostics: vscode.Diagnostic[] = [];
-            let diagnostic: vscode.Diagnostic;
+            
+            let basicUri = editor.document.uri;
+            let basicPath = basicUri.path;
+            basicPath = basicPath.slice(0, basicPath.lastIndexOf('/'));
 
-            for (let i = 0; i < numberOfErrors; i++) {
-                diagnostic = new vscode.Diagnostic(new vscode.Range(0, 0, 0, 0), `error #${i} on ${source}`, vscode.DiagnosticSeverity.Error);
-                diagnostic.source = source;
-                diagnostics.push(diagnostic);
-                diagnosticCollection.set(editor.document.uri, diagnostics);
+            for (let i = 0; i < numberOfFiles; i++) {
+                let diagnostics: vscode.Diagnostic[] = [];
+                let diagnostic: vscode.Diagnostic;
+                let uri = i === 0 ? basicUri : basicUri.with({ path: `${basicPath}/file-${i}.js` });
+                for (let j = 0; j < numberOfErrors; j++) {
+                    diagnostic = new vscode.Diagnostic(new vscode.Range(0, 0, 0, 0), `error #${j}-${i} on ${source}`, vscode.DiagnosticSeverity.Error);
+                    diagnostic.source = source;
+                    diagnostics.push(diagnostic);
+                    diagnosticCollection.set(uri, diagnostics); // this call is intentionally inside the loop to simulate a programming error
+                }
             }
         }    
     }
@@ -92,13 +101,27 @@ export function activate(context: vscode.ExtensionContext) {
         if (editor) {
             vscode.commands.executeCommand('workbench.action.output.toggleOutput')
                 .then(() => vscode.commands.executeCommand('workbench.actions.view.problems'))
-                .then(() => vscode.window.showInputBox({ value: NUMBER_OF_ERRORS.toString(), prompt: 'Number of errors to generate' }))
+                .then(() => vscode.window.showInputBox({ value: NUMBER_OF_ERRORS_LARGE.toString(), prompt: 'Number of errors to generate' }))
                 .then(value => parseInt(value))
                 .then(numberOfErrors => floodDiagnostics(editor, numberOfErrors));
         }
     });
     context.subscriptions.push(disposable);
 
+    disposable = vscode.commands.registerCommand('crazy.floodFileDiagnostics', () => {
+        let editor = vscode.window.activeTextEditor;
+        if (editor) {
+            let numberOfFiles = NUMBER_OF_FILES;
+            vscode.commands.executeCommand('workbench.action.output.toggleOutput')
+                .then(() => vscode.commands.executeCommand('workbench.actions.view.problems'))
+                .then(() => vscode.window.showInputBox({ value: NUMBER_OF_FILES.toString(), prompt: 'Number of files with errors'}))
+                .then(value => numberOfFiles = parseInt(value))
+                .then(() => vscode.window.showInputBox({ value: NUMBER_OF_ERRORS_SMALL.toString(), prompt: 'Number of errors per file' }))
+                .then(value => parseInt(value))
+                .then(numberOfErrors => floodDiagnostics(editor, numberOfErrors, numberOfFiles));
+        }
+    });
+    context.subscriptions.push(disposable);
 
     disposable = vscode.commands.registerCommand('crazy.clearDiagnostics', () => {
         diagnosticCollections.forEach(c => c.dispose());
